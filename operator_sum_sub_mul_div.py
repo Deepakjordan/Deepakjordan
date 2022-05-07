1f070352c0db1f9849b87fedfbaed2f47086d6c6
DEEPAK <104426754+Deepakjordan@users.noreply.github.com>
2022-05-07 20:33:19 +0530

    Add files via upload
    Addition_subtraction_multification_division (of two number)

---
+++ operator_sum_sub_mul_div.py
@@ -0,0 +1,9 @@
+def main():
+    num1=int(input("enter a number:"))
+    num2=int(input("enter another number:"))
+    print(f'{num1} + {num2} ={num1 + num2}')
+    print(f'{num1} - {num2} ={num1 - num2}')
+    print(f'{num1} * {num2} ={num1 * num2}')
+    print(f'{num1} / {num2} ={num1 / num2}')
+
+main()    
