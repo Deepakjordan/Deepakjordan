b3e8d2e54401378087f6e8524f37087ea51ca6d7
DEEPAK <104426754+Deepakjordan@users.noreply.github.com>
2022-05-08 21:03:43 +0530

    Add files via upload

---
+++ prime_number_given_range.py
@@ -0,0 +1,15 @@
+def main():
+    lower = int(input("enter the 1st range:"))
+    upper = int(input("enter the 2nd range:"))
+
+    for num in range(lower, upper + 1):
+    
+        # all prime numbers are greater than 1
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+
+            else:
+                print(num)
+main()
---
+++ reverse_number.py
@@ -0,0 +1,14 @@
+def main():
+    num = int(input('enter a number:'))
+    display_reverse(num)
+
+def display_reverse(num):
+    reverse_num=0
+
+    while num != 0:
+        digit = num % 10
+        reverse_num = reverse_num * 10 + digit
+        num //= 10
+    print(int(reverse_num))
+
+main()
